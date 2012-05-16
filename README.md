dc301cc3d6cbdff5e8c548f727940ef82ab9f3da
Phaiax <phaiax@invisibletower.de>
2012-05-16 21:34:06 +0200

    initial commit

--- README.md
+++ README.md
@@ -2,3 +2,16 @@ asfu
 ====
 
 automatic ssh file uploader
+
+
+Usage:
+./asfud.sh <directory-to-monitor> <ssh host> <url>
+
+ssh host example: marc@myhost:/home/mark/www/screenshots
+
+
+You need to have installed 
+ * xclip
+ * inotify-tools
+ 
+ With ubuntu, you get a notification via notify send. Please extend and push your changes.
---
+++ asfud.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+WATCH_DIR=$1/
+SSH_PATH=$2/
+URL_BASE=$3
+
+SAVEIFS=$IFS
+IFS="
+"
+echo $IFS
+
+echo $WATCH_DIR
+echo $SSH_PATH
+echo $URL_BASE
+
+NOTIFY=$(which notify-send)
+
+while inotifywait -e close_write $WATCH_DIR; do
+  FILE=$(dir -t -1 --quoting-style=literal  $WATCH_DIR | head -n 1)
+  FILE_WITHOUT_SPACES=$(echo $FILE | sed 's/ //g')
+  sleep 1
+  LOCAL_FILE=$WATCH_DIR$FILE
+  scp$IFS$LOCAL_FILE$IFS$SSH_PATH$FILE_WITHOUT_SPACES
+  URL=$URL_BASE$FILE_WITHOUT_SPACES
+  echo $URL | xclip -i -selection clipboard
+  if [ "$NOTIFY" != "" ] ; then
+    notify-send "Screenshot upload" "Link copied to clipboard\n$URL"
+  fi
+  
+done
+
+IFS=$SAVEIFS
